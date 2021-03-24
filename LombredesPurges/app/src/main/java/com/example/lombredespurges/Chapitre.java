90562f60658ac8193d7a69264f5a1d4de76e6952
msorroche <msorroche@dept-info.crosemont.quebec>
2021-03-24 15:18:46 -0400

    ajout valeur string.xml et changement d'image

--- LombredesPurges/app/src/main/java/com/example/lombredespurges/Chapitre.java
+++ LombredesPurges/app/src/main/java/com/example/lombredespurges/Chapitre.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Chapitre extends Fragment {
+
+    //TextView texteRecu;
+    ImageView raceChap;
 
     public Chapitre() {
         // Required empty public constructor
@@ -21,6 +26,19 @@ public class Chapitre extends Fragment {
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public void changerImage() {
+
+        String nomRace = getArguments().getString("nomRace");
+
+        if (nomRace.equals("via")) {
+            raceChap.setImageDrawable(getResources().getDrawable(R.drawable.via));
+        } else if (nomRace.equals("kaqchikam")) {
+            raceChap.setImageDrawable(getResources().getDrawable(R.drawable.kaqchikam));
+        } else if (nomRace.equals("dino")) {
+            raceChap.setImageDrawable(getResources().getDrawable(R.drawable.dinoh));
+        }
     }
 
     @Override
@@ -38,5 +56,13 @@ public class Chapitre extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        raceChap = view.findViewById(R.id.raceChap);
+
+        changerImage();
+
+        /*String nom = getArguments().getString("nom");
+        texteRecu = view.findViewById(R.id.texteRecu);
+        texteRecu.setText(nom);*/
     }
 }
