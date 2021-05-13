abb0b236d24bb759783008e2e52dde99ba80f562
William Alexand Hernandez <whernandez@dept-info.crosemont.quebec>
2021-05-13 18:33:33 -0400

    Ajouter les classe SourceDeDonnées sans des methodes

---
+++ LombredesPurges/app/src/main/java/com/example/lombredespurges/source_de_données/SourceDeDonnéesHTTP.java
@@ -0,0 +1,15 @@
+package com.example.lombredespurges.source_de_données;
+
+import com.example.lombredespurges.domaine.interacteur.SourceDeDonnées;
+
+public class SourceDeDonnéesHTTP implements SourceDeDonnées {
+    @Override
+    public void enregistrer() {
+
+    }
+
+    @Override
+    public void récupérer() {
+
+    }
+}
